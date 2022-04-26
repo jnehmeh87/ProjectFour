3a4a12e24e3d0d7f5fb396002a8f95d1fb2bf9d1
jnehmeh87 <jadnehmeh.87@gmail.com>
2022-04-26 12:25:29 +0000

    Create comments models

--- recipe/models.py
+++ recipe/models.py
@@ -26,4 +26,15 @@ class Recipe(models.Model):
         return self.likes.count()
 
 class Comments(models.Model):
-    
+    post = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = model.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"

4aeb237b4df1fd3be7f2825f7b022cd8d6e60b8d
jnehmeh87 <jadnehmeh.87@gmail.com>
2022-04-26 18:32:37 +0000

    add Recipe and Comments to admin.py

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, Comments
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Recipe)
 class PostAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'preparation_content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('preparation_content', 'ingredients')
+
+
+@admin.register(Comments)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name', 'email', 'body']
+    actions = ['approved_comments']
+
+    def approved_comments(self, request, queryset):
+        queryset.update(approved=True)
